73d52bea797ff68320708ad94cc6f222ff34e9b6
sauce-Git <dadad1004@naver.com>
2024-12-18 01:30:52 +0900

    feat move cors option to gateway

--- src/main/java/com/tarot/core/config/SecurityConfig.java
+++ src/main/java/com/tarot/core/config/SecurityConfig.java
@@ -1,26 +1,17 @@
 package com.tarot.core.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
 
-  private final CorsConfigurationSource corsConfigurationSource;
-
-  public SecurityConfig(@Qualifier("corsConfig") CorsConfigurationSource corsConfigurationSource) {
-    this.corsConfigurationSource = corsConfigurationSource;
-  }
-
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource))
-        .csrf(AbstractHttpConfigurer::disable);
+    http.csrf(AbstractHttpConfigurer::disable);
 
     return http.build();
   }
